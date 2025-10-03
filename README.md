dba2413caee3dd4bb8ce66cb925a55a90a911f61
Aravind <gmarav005@gmail.com>
2025-10-03 22:57:01 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Library
+
+It is a library where you can enter Book title, author, number of pages and checkbox whether you have finished reading or not.
+
+## Features
+- New Book
+- Book title
+- Author
+- Pages
+- Read
+- Remove
+
+## Learnings
+- How to use grid to layout cards of different books.
+- Learned how to use constructors and dialogs.
+- DOM Manipulations.
+
+## Image
+
+ ![](assets/screenshot.png)
+
+## Live demo
+
+[ View Live  :)](https://gmarav05.github.io/Library/)
+ 
--- app.js
+++ app.js
@@ -5,112 +5,101 @@ const addBookBtn = document.getElementById("addBookBtn");
 const cardsContainer = document.getElementById("cardsContainer");
 const form = document.getElementById("form");
 
+// show modal
 
-// show modal 
-
-showButton.addEventListener("click" , () => { 
-    dialog.showModal();
+showButton.addEventListener("click", () => {
+  dialog.showModal();
 });
 
 // close modal
 
-closeButton.addEventListener("click" , () => {
-    dialog.close();
+closeButton.addEventListener("click", () => {
+  dialog.close();
 });
 
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.bookId = crypto.randomUUID(),
-    this.bookTitle = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read;
+  (this.bookId = crypto.randomUUID()),
+    (this.bookTitle = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);   
-    displayBooks();
+  myLibrary.push(book);
+  displayBooks();
 }
 
 Book.prototype.setRead = function () {
-    this.read = !this.read;
-}
+  this.read = !this.read;
+};
 
-addBookBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+addBookBtn.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    const newBook = new Book(
-        document.getElementById("bookName").value,
-        document.getElementById("author").value,
-        document.getElementById("pages").value,
-        document.getElementById("read").checked
-    );
-
-    console.log(newBook);
-    addBookToLibrary(newBook);
-    // displayBooks(newBook);
-    dialog.close();
-    form.reset();
-    
-})
+  const newBook = new Book(
+    document.getElementById("bookName").value,
+    document.getElementById("author").value,
+    document.getElementById("pages").value,
+    document.getElementById("read").checked
+  );
+  addBookToLibrary(newBook);
+  dialog.close();
+  form.reset();
+});
 
 function displayBooks() {
-    cardsContainer.innerHTML = "";
-    
-    myLibrary.forEach((book) => {
+  cardsContainer.innerHTML = "";
 
-        const bookCard = document.createElement('div');
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
 
-        bookCard.classList.add('card');
+    bookCard.classList.add("card");
 
-        bookCard.setAttribute('data-id', book.bookId);
+    bookCard.setAttribute("data-id", book.bookId);
 
-        const bookName = document.createElement('h3');
-        bookName.textContent = `${book.bookTitle}`;
+    const bookName = document.createElement("h3");
+    bookName.textContent = `${book.bookTitle}`;
 
-        const author = document.createElement('p');
-        author.textContent = `Author: ${book.author}`;
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book.author}`;
 
-        const pages = document.createElement('p');
-        pages.textContent = `Pages: ${book.pages}`;
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${book.pages}`;
 
-        const status = document.createElement('p');
-        status.textContent = `Status: ${book.read ? "Read" : "Not Read"}`;
+    const status = document.createElement("p");
+    status.textContent = `Status: ${book.read ? "Read" : "Not Read"}`;
 
-
-        const readButton = document.createElement("button");
-        readButton.classList.add("read-button");
-        readButton.textContent = 'Set Read';
-        readButton.addEventListener('click', function() {
-            book.setRead();
-            displayBooks();
-        });
-
-        const removeButton = document.createElement("button");
-        removeButton.classList.add("remove-button");
-        removeButton.textContent = 'Remove Book';
-        removeButton.addEventListener('click', function() {
-            const index = myLibrary.findIndex(b => b.bookId === book.bookId);
-            if (index !== -1) {
-                myLibrary.splice(index,1);
-                displayBooks();
-            }
-        });
-
-        bookCard.appendChild(bookName);
-        bookCard.appendChild(author);
-        bookCard.appendChild(pages);
-        bookCard.appendChild(status);
-        bookCard.appendChild(readButton);
-        bookCard.appendChild(removeButton);
-
-        
-        cardsContainer.appendChild(bookCard);
-
+    const readButton = document.createElement("button");
+    readButton.classList.add("read-button");
+    readButton.textContent = "Set Read";
+    readButton.addEventListener("click", function () {
+      book.setRead();
+      displayBooks();
     });
-    
+
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    removeButton.textContent = "Remove Book";
+    removeButton.addEventListener("click", function () {
+      const index = myLibrary.findIndex((b) => b.bookId === book.bookId);
+      if (index !== -1) {
+        myLibrary.splice(index, 1);
+        displayBooks();
+      }
+    });
+
+    bookCard.appendChild(bookName);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(status);
+    bookCard.appendChild(readButton);
+    bookCard.appendChild(removeButton);
+
+    cardsContainer.appendChild(bookCard);
+  });
 }
 
-
-addBookToLibrary(new Book('arav', 'Intro to JS', 150, read));
+addBookToLibrary(new Book("Cosmos", "Carl Sagan", 396, read));
