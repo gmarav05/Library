8f6a1874fd878db870509e30a79cb3622939ab90
Aravind <gmarav005@gmail.com>
2025-10-03 19:15:40 +0530

    book creation functionality is working correctly

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+const dialog = document.querySelector("dialog");
 const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-// const outputBox = document.querySelector("output");
-// const selectEl = favDialog.querySelector("select");
 const closeButton = document.getElementById("close");
-// const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+// show modal 
 
 showButton.addEventListener("click" , () => { 
-    favDialog.showModal();
+    dialog.showModal();
 });
 
-
+// close modal
 
 closeButton.addEventListener("click" , () => {
-    favDialog.close();
-});
+    dialog.close();
 
+    outputBox.value = dialog.returnValue === "default"
+      ? "No return value."
+      :`ReturnValue:
+      ${dialog.returnValue}.`;
+});
 
 const myLibrary = [];
 
@@ -28,8 +31,58 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
-    
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);   
 }
 
-console.log(Book());
+Book.prototype.setRead = function () {
+    this.read = !this.read;
+}
+
+addBookBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const newBook = new Book(
+        3, 
+        document.getElementById("bookName").value,
+        document.getElementById("author").value,
+        document.getElementById("pages").value,
+        document.getElementById("read").value
+    );
+
+    console.log(newBook);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const displayBooks = () => {
+    card.innerHTML = "";
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const addCard = document.createElement("div");
+        const book = myLibrary[i];
+        addCard.classList.add("book");
+        const deleteCard = document.createElement("button");
+        deleteCard.innerText = "\u00D7";
+        deleteCard.addEventListener('click', () => {
+            if (window.confirm("Sure you want to delete this card?")) {
+                myLibrary.splice(i,1);
+                displayBooks();
+            }
+        })
+        
+    }
+
+}
